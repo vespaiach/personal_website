4f7f014f93f2f7eba0da2b30d0572ff65d28c33e
Toan Nguyen <nta.toan@gmail.com>
2022-06-09 18:45:37 -0400

    chore: update gtag id

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,13 +13,13 @@ export default function Home({ posts }: { posts: PostData[] }) {
       <Head>
         <title>Learn to share - by Vespaiach</title>
 
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_GTAG_ID}`}></script>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG_ID}`}></script>
         <script dangerouslySetInnerHTML={{
             __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_GTAG_ID}');
+            gtag('config', '${process.env.NEXT_PUBLIC_GTAG_ID}');
             `
         }}/>
 
