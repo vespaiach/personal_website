2a76d9f9196901bf844623192cd238da373a489c
Toan Nguyen <nta.toan@gmail.com>
2022-06-26 10:00:00 -0400

    chore: move gtag to document page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
@@ -6,6 +7,24 @@ class MyDocument extends Document {
             <Html lang="en" className="scroll-smooth">
                 <Head>
                     <link rel="stylesheet" href="/main.css" />
+                    {process.env.NEXT_PUBLIC_GTAG_ID && (
+                        <>
+                            <Script
+                                async
+                                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG_ID}`}
+                            />
+                            <Script
+                                dangerouslySetInnerHTML={{
+                                    __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GTAG_ID}');
+            `,
+                                }}
+                            />
+                        </>
+                    )}
                 </Head>
                 <body className="bg-white text-gray-500 antialiased dark:bg-gray-900 dark:text-white">
                     <Main />
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -9,26 +9,7 @@ export default function Home({ posts }: { posts: SerializedPostData[] }) {
     return (
         <Layout>
             <Head>
-                <title>Posts - Vespaiach</title>
-
-                {process.env.NEXT_PUBLIC_GTAG_ID && (
-                    <>
-                        <script
-                            async
-                            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG_ID}`}></script>
-                        <script
-                            dangerouslySetInnerHTML={{
-                                __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GTAG_ID}');
-            `,
-                            }}
-                        />
-                    </>
-                )}
-
+                <title>Posts - Nguyen's Blog</title>
                 <meta
                     name="description"
                     content="Knowledge sharing is the way to make our life better, and that is all this website about."
