f18c592581ed1d392fb591dd5b985953effc72a4
Toan Nguyen <nta.toan@gmail.com>
2025-03-16 23:45:24 -0400

    add syntax highlight

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className="prose lg:prose-xl dark:prose-invert p-5">{children}</body>
+      <body className="prose prose-stone lg:prose-xl dark:prose-invert p-5">{children}</body>
     </html>
   )
 }
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,24 +1,13 @@
 import { getPostBySlug } from 'shared/db'
 import { notFound } from 'next/navigation'
-import Link from 'next/link'
+import Article from '@/app/ui/Article'
 
 export default async function PostPage({ params }: { params: { slug: string } }) {
-  const post = await getPostBySlug(params.slug)
+  const post = await getPostBySlug(await params.slug)
 
   if (!post) {
     notFound()
   }
 
-  return (
-    <main>
-      <Link href="/">
-        <small>← back</small>
-      </Link>
-      <h1 className="mt-6">{post.title}</h1>
-      <article dangerouslySetInnerHTML={{ __html: post.content }} />
-      <Link href="/">
-        <small>← back</small>
-      </Link>
-    </main>
-  )
+  return <Article post={post} /> 
 }
