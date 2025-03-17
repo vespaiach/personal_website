1f1436a04385d5e0400b671095f42673eb643534
Toan Nguyen <nta.toan@gmail.com>
2025-03-16 23:55:41 -0400

    fix param type error

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -2,12 +2,14 @@ import { getPostBySlug } from 'shared/db'
 import { notFound } from 'next/navigation'
 import Article from '@/app/ui/Article'
 
-export default async function PostPage({ params }: { params: { slug: string } }) {
-  const post = await getPostBySlug(await params.slug)
+type Params = Promise<{ slug: string }>
+
+export default async function PostPage({ params }: { params: Params }) {
+  const post = await getPostBySlug((await params).slug)
 
   if (!post) {
     notFound()
   }
 
-  return <Article post={post} /> 
+  return <Article post={post} />
 }
