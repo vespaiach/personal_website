366fc1573ab1b40395f9196abd84d02f57246c14
Toan Nguyen <nta.toan@gmail.com>
2025-03-27 22:32:24 -0400

    move to prism

--- public/highlight-code-worker.js
+++
@@ -1,9 +0,0 @@
-onmessage = (event) => {
-	const { language, text } = event.data;
-  const lang = language.replace('language-', '');
-  importScripts('/highlight.min.js');
-  importScripts(`/languages/${lang}.min.js`);
-  const result = self.hljs.highlight(text, { language: lang, ignoreIllegals: true });
-	postMessage({ highlightedCode: result.value, language: lang, rawCode: text });
-  close();
-};
