7f7e59dd00665de0d7ebc63d38adf7447355a907
Toan Nguyen <nta.toan@gmail.com>
2025-04-02 23:01:23 -0400

    minify

--- src/utils.ts
+++ src/utils.ts
@@ -35,6 +35,6 @@ export async function minify(src: string) {
     minifyCSS: true,
     minifyJS: true,
     minifyURLs: true,
-    // processScripts: ['application/ld+json']
+    processScripts: ['application/ld+json']
   })
 }
