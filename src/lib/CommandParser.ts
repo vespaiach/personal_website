104f5149dab5a0977aaf479ffe58eef0fb91c354
Trinh Nguyen <nta.toan@gmail.com>
2026-09-19 12:04:05 -0400

    chore: lowercase

--- src/lib/CommandParser.ts
+++ src/lib/CommandParser.ts
@@ -1,5 +1,6 @@
 export function parseCommand(inputPrompt: string): string[] {
   return inputPrompt
+    .toLowerCase()
     .split("&&")
     .map((command) => command.trim().replace(/\s+/g, " "))
     .filter((command) => command.length > 0);
